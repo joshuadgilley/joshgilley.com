26cfacc729a3c1e9e3594511cd29012b2e11d663
Josh Gilley <gilleyjd@g.cofc.edu>
2023-01-04 16:06:07 -0500

    added markdown read in blog post capability, still needs work

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,50 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+const fs = require("fs");
+const readMultipleFiles = require("read-multiple-files");
 
 type Data = {
-  name: string
-}
+  [key: string]: string;
+};
+
+// const readFiles = async (dir: any, cb: any) => {
+//   const obj: Data = {};
+//   await fs.readdir(dir, (err: any, files: any) => {
+//     if (err) {
+//       throw err;
+//     }
+
+//     files.forEach(async (file: any) => {
+//       console.log(file);
+//       const date: string = file.split("_")[1].split(".")[0];
+//       await fs.readFile(
+//         `./blogposts/${file}`,
+//         "utf8",
+//         (err: any, data: any) => {
+//           obj[date] = data;
+//           console.log("flag1");
+//         }
+//       );
+//     });
+//   });
+//   console.log("flag2");
+//   cb(obj);
+// };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const dir: string = "./blogposts";
+  let files = ["test_01-04-2023"];
+  readMultipleFiles(files, "utf8", function (err: any, inputFiles: any) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(inputFiles);
+  });
+  res.status(200).json({ name: "John" });
+
+  // readFiles(dir, function (data: any) {
+  // });
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import React from "react";
+import Markdown from "react-markdown";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Blog() {
+export default function Blog(props: any) {
   return (
     <>
       <Head>
@@ -14,11 +14,18 @@ export default function Blog() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-            <p>Blog coming soon..&nbsp;</p>
-        </div>
-      </main>
+      <div>
+        <Markdown>{props.children}</Markdown>
+      </div>
     </>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const children = require("../blogposts/test.md").default;
+  return {
+    props: {
+      children,
+    },
+  };
 }

7975bb7abd803acef5d63b406a928c0523d69a4b
Josh Gilley <jgilley@scpdist.com>
2023-01-06 10:44:02 -0500

    Contact form working with SendGrid (now need to clean up code, consider tailwind and style for responsive

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 const sgMail = require('@sendgrid/mail')
-import getHtmlString from '../../helpers/HtmlString';
+import getHtmlString from '../../templates/HtmlString';
 
 type Data = Error | string;
 

95d6d48288793d6783a2c2b8222fc2d262710abd
Josh Gilley <jgilley@scpdist.com>
2023-01-06 15:33:21 -0500

    changed footer

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -177,7 +177,7 @@ export default function Layout({ children }: { children: any }) {
          {/* Footer */}
          <footer className="bottom-0 w-full bg-white dark:bg-gray-900">
           <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-              <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
+              <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">Joshua Gilley</a>. All Rights Reserved.
               </span>
               <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0">
                   <a href="#" className="text-gray-400 hover:text-gray-900 dark:hover:text-white">

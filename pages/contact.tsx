9541095d98991b8ad6192f321713307861c7feac
Josh Gilley <jgilley@scpdist.com>
2023-01-10 12:02:30 -0500

    fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -75,11 +75,11 @@ export default function Contact() {
         <div className={styles.description}>
           <section className="bg-white dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">I'd love to hear from</p>
+                <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{"Contact Me"}</h2>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{"I'd love to hear from"}</p>
                 <form className="space-y-8" onSubmit={(e) => formSubmit(e)}>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{"Your Email"}</label>
                         <input 
                           type="email" 
                           id="email" 
@@ -91,7 +91,7 @@ export default function Contact() {
                         />
                     </div>
                     <div>
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{"Subject"}</label>
                         <input 
                           type="text" 
                           id="subject" 
@@ -103,7 +103,7 @@ export default function Contact() {
                         />
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your Message</label>
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{"Your Message"}</label>
                         <textarea 
                           id="message" 
                           rows={6} 
@@ -112,7 +112,7 @@ export default function Contact() {
                           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
                           placeholder="Leave a comment..."></textarea>
                     </div>
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>                
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{"Button"}</button>                
                   </form>
             </div>
           </section>

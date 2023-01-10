14d95dfbc915324578ea3bb9fa5787868a096693
Josh Gilley <jgilley@scpdist.com>
2023-01-10 14:59:25 -0500

    More styling updates

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -68,23 +68,23 @@ export default function Contact({endpoint}: InferGetStaticPropsType<typeof getSt
           {emailError ? <EmailErrorAlert /> : ""}
           <section className="dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{"Contact Me"}</h2>
+                <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-800 dark:text-white">{"Contact Me"}</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{"I'd love to hear from"}</p>
                 <form className="space-y-8" onSubmit={(e) => formSubmit(e)}>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{"Your Email"}</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-300">{"Your Email"}</label>
                         <input 
                           type="email" 
                           id="email" 
                           value={email} 
                           onChange={(e) => setEmail(e.target.value)} 
-                          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
                           placeholder="name@gmail.com" 
                           required 
                         />
                     </div>
                     <div>
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{"Subject"}</label>
+                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-300">{"Subject"}</label>
                         <input 
                           type="text" 
                           id="subject" 
@@ -96,7 +96,7 @@ export default function Contact({endpoint}: InferGetStaticPropsType<typeof getSt
                         />
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{"Your Message"}</label>
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-400">{"Your Message"}</label>
                         <textarea 
                           id="message" 
                           rows={6} 
@@ -105,7 +105,7 @@ export default function Contact({endpoint}: InferGetStaticPropsType<typeof getSt
                           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
                           placeholder="Leave a comment..."></textarea>
                     </div>
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{buttonText}</button>                
+                    <button type="submit" className="mb-3 bg-gray-800 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white">{buttonText}</button>                
                   </form>
             </div>
           </section>

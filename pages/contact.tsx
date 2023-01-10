30383c298082b69962bc58d18008b64f4abcd9cf
Josh Gilley <jgilley@scpdist.com>
2023-01-10 16:10:37 -0500

    love to hear from you

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -69,7 +69,7 @@ export default function Contact({endpoint}: InferGetStaticPropsType<typeof getSt
           <section className="dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-800 dark:text-white">{"Contact Me"}</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{"I'd love to hear from"}</p>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{"I'd love to hear from you"}</p>
                 <form className="space-y-8" onSubmit={(e) => formSubmit(e)}>
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-300">{"Your Email"}</label>

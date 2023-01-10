af68de4cf949987a13099393743f4f459913010f
Josh Gilley <jgilley@scpdist.com>
2023-01-10 11:59:09 -0500

    Tailwind applied to contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Contact.module.css";
 import { TextField } from "@mui/material";
 import React, { useState } from "react";
 import axios from "axios";
@@ -54,7 +54,7 @@ export default function Contact() {
     };
 
     try {
-      const res = await axios.post(`/api/SendEmail`, data);
+      const res = await axios.post(`${process.env.SENDEMAIL_ENDPOINT}`, data);
       console.log(res);
       setSent(true);
       resetForm();
@@ -73,69 +73,50 @@ export default function Contact() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <form className="contact-form" onSubmit={(e) => formSubmit(e)}>
-            <TextField
-              id="standard-multiline-flexible"
-              label="Message"
-              placeholder="Enter Message"
-              variant="outlined"
-              multiline
-              //   rowsMax={4}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-              type="text"
-            />
-            <br />
-            <br />
-            <br />
-
-            <TextField
-              id="outlined-basic"
-              placeholder="Enter your name"
-              label="Name"
-              variant="outlined"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              type="text"
-            />
-            <br />
-            <br />
-            <br />
-
-            <TextField
-              id="outlined-basic"
-              label="Email"
-              placeholder="Enter email address"
-              variant="outlined"
-              value={email}
-              onChange={(e) => handleChangeEmail(e)}
-              error={emailError}
-              required
-              type="email"
-            />
-            <br />
-            <br />
-            <br />
-            <TextField
-              id="outlined-basic"
-              placeholder="Enter Subject"
-              label="Subject"
-              variant="outlined"
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
-              required
-            />
-            <br />
-            <br />
-            <br />
-            <div className="button--container">
-              <button type="submit" className="button button-primary">
-                {buttonText}
-              </button>
+          <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">I'd love to hear from</p>
+                <form className="space-y-8" onSubmit={(e) => formSubmit(e)}>
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
+                        <input 
+                          type="email" 
+                          id="email" 
+                          value={email} 
+                          onChange={(e) => setEmail(e.target.value)} 
+                          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                          placeholder="name@gmail.com" 
+                          required 
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                        <input 
+                          type="text" 
+                          id="subject" 
+                          value={subject} 
+                          onChange={(e) => setSubject(e.target.value)} 
+                          className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                          placeholder="Let us know how we can help you" 
+                          required 
+                        />
+                    </div>
+                    <div className="sm:col-span-2">
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your Message</label>
+                        <textarea 
+                          id="message" 
+                          rows={6} 
+                          value={message} 
+                          onChange={(e) => setMessage(e.target.value)}  
+                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                          placeholder="Leave a comment..."
+                        ></textarea>
+                    </div>
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>                
+                  </form>
             </div>
-          </form>
+          </section>
         </div>
       </main>
     </>

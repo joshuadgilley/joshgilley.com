d08da10d4b2b43b45cc44e2d587880aeb9b186d6
Josh Gilley <jgilley@scpdist.com>
2023-01-10 14:18:48 -0500

    Added background image

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -66,7 +66,7 @@ export default function Contact({endpoint}: InferGetStaticPropsType<typeof getSt
       <main className={styles.main}>
         <div className={styles.description}>
           {emailError ? <EmailErrorAlert /> : ""}
-          <section className="bg-white dark:bg-gray-900">
+          <section className="dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">{"Contact Me"}</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{"I'd love to hear from"}</p>

76bb022afa0c199fd83cd99d30b337c87f038893
Josh Gilley <jgilley@scpdist.com>
2023-01-10 16:45:17 -0500

    added josh image

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/About.module.css";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -13,23 +14,34 @@ export default function About() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Software engineer based out of Charleston, SC with a computer
-            science degree and over 2 years of experience in the industry.
-            Proficient in a variety of programming languages including
-            JavaScript, HTML, CSS, and React.js. Experienced in developing and
-            maintaining both front-end and back-end web applications. Strong
-            problem-solving skills and the ability to effectively communicate
-            technical concepts to both technical and non-technical audiences.&nbsp;
-          </p>
-          <p>
-          Able to work independently as well as part of a team, and have a
-            strong focus on delivering high-quality products. In my previous
-            roles, I have worked on a variety of projects ranging from small
-            business websites to large-scale e-commerce platforms. I am
-            constantly seeking to learn and stay up-to-date on the latest
-            technologies and industry best practices.&nbsp;
-          </p>
+          <div>
+            <Image
+              src="/../public/images/josh.jpg"
+              alt="ItMe"
+              width={300}
+              height={500}
+              className={styles.img}
+            />
+          </div>
+          <div className={styles.about}>
+            <p>
+              Software engineer based out of Charleston, SC with a computer
+              science degree and over 2 years of experience in the industry.
+              Proficient in a variety of programming languages including
+              JavaScript, HTML, CSS, and React.js. Experienced in developing and
+              maintaining both front-end and back-end web applications. Strong
+              problem-solving skills and the ability to effectively communicate
+              technical concepts to both technical and non-technical audiences.&nbsp;
+            </p>
+            <p>
+            Able to work independently as well as part of a team, and have a
+              strong focus on delivering high-quality products. In my previous
+              roles, I have worked on a variety of projects ranging from small
+              business websites to large-scale e-commerce platforms. I am
+              constantly seeking to learn and stay up-to-date on the latest
+              technologies and industry best practices.&nbsp;
+            </p>
+          </div>
         </div>
       </main>
     </>

5b9adececfbec46f5bf222dade6a8dbe96d1f641
Josh Gilley <jgilley@scpdist.com>
2023-01-10 16:15:05 -0500

    removed some dark mode tailwind in contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -72,37 +72,37 @@ export default function Contact({endpoint}: InferGetStaticPropsType<typeof getSt
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{"I'd love to hear from you"}</p>
                 <form className="space-y-8" onSubmit={(e) => formSubmit(e)}>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-300">{"Your Email"}</label>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800">{"Your Email"}</label>
                         <input 
                           type="email" 
                           id="email" 
                           value={email} 
                           onChange={(e) => setEmail(e.target.value)} 
-                          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " 
                           placeholder="name@gmail.com" 
                           required 
                         />
                     </div>
                     <div>
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-300">{"Subject"}</label>
+                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-800 ">{"Subject"}</label>
                         <input 
                           type="text" 
                           id="subject" 
                           value={subject} 
                           onChange={(e) => setSubject(e.target.value)} 
-                          className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                          className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" 
                           placeholder="Let us know how we can help you" 
                           required 
                         />
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-800 dark:text-gray-400">{"Your Message"}</label>
+                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-800">{"Your Message"}</label>
                         <textarea 
                           id="message" 
                           rows={6} 
                           value={message} 
                           onChange={(e) => setMessage(e.target.value)}  
-                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" 
                           placeholder="Leave a comment..."></textarea>
                     </div>
                     <button type="submit" className="mb-3 bg-gray-800 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white">{buttonText}</button>                

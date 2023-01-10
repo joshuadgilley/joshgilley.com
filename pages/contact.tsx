1ffd10c373b44aeea88382b4d602ba4df4da2bdc
Josh Gilley <jgilley@scpdist.com>
2023-01-10 12:00:58 -0500

    fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -110,8 +110,7 @@ export default function Contact() {
                           value={message} 
                           onChange={(e) => setMessage(e.target.value)}  
                           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
-                          placeholder="Leave a comment..."
-                        ></textarea>
+                          placeholder="Leave a comment..."></textarea>
                     </div>
                     <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>                
                   </form>

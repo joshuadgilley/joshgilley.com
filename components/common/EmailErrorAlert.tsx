60bbd57777a1b0145810ed209057cf7ae57f0b3b
Josh Gilley <jgilley@scpdist.com>
2023-01-10 13:29:35 -0500

    styling the alert button

--- components/common/EmailErrorAlert.tsx
+++ components/common/EmailErrorAlert.tsx
@@ -1,11 +1,11 @@
 export default function EmailErrorAlert() {
     return (
-        <div role="alert" className="w-1/4 m-2 transition ease-in-out delay-150">
+        <div role="alert" className="absolute w-1/4 m-2 transition duration-100 ease-in-out">
             <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
                 Error
             </div>
             <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                <p>Something not ideal might be happening.</p>
+                <p>The email did not send..</p>
             </div>
         </div>
     )

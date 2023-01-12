f68808308201daca0e180c51b2e072c1e08897e8
Josh Gilley <jgilley@scpdist.com>
2023-01-12 16:34:45 -0500

    ReCaptcha added

--- components/common/EmailErrorAlert.tsx
+++ components/common/EmailErrorAlert.tsx
@@ -1,11 +1,11 @@
-export default function EmailErrorAlert() {
+export default function EmailErrorAlert({text}: {text: string}) {
     return (
         <div role="alert" className="absolute w-1/4 m-2 transition duration-100 ease-in-out">
             <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
                 Error
             </div>
             <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                <p>The email did not send..</p>
+                <p>{text}</p>
             </div>
         </div>
     )
--- pages/api/SendEmail.ts
+++ pages/api/SendEmail.ts
@@ -2,35 +2,49 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 const sgMail = require('@sendgrid/mail')
 import getHtmlString from '../../templates/html_string';
+const axios = require('axios');
 
 type Data = Error | string;
 
-export default function handler(
+const handleRecaptcha = async (token: string) => {
+  const secret_key = process.env.RECAPTCHA_SECRET_KEY;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
+
+  const res = await axios.post(url);
+  return res;
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const data = req.body;
-  const { subject } = data;
-  const image = "https://media.istockphoto.com/id/1328032651/vector/cartoon-white-bubble-with-email-notice.jpg?s=612x612&w=0&k=20&c=Olo9PmvJezFAu5mdUHnulKsSpjlTpEVrgvlu2FEevrU=";
-  data.image = image;
-  const htmlString = getHtmlString(data);
+  const recapRes = await handleRecaptcha(req.body.token);
+  if (recapRes.data.success) {
+    const data = req.body;
+    const { subject } = data;
+    const image = "https://media.istockphoto.com/id/1328032651/vector/cartoon-white-bubble-with-email-notice.jpg?s=612x612&w=0&k=20&c=Olo9PmvJezFAu5mdUHnulKsSpjlTpEVrgvlu2FEevrU=";
+    data.image = image;
+    const htmlString = getHtmlString(data);
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  const msg = {
-    to: process.env.PERSONAL_EMAIL, // Change to your recipient
-    from: process.env.PERSONAL_EMAIL, // Change to your verified sender
-    subject: subject,
-    // text: 'and easy to do anywhere, even with Node.js'
-    html: htmlString,
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: process.env.PERSONAL_EMAIL, // Change to your recipient
+      from: process.env.PERSONAL_EMAIL, // Change to your verified sender
+      subject: subject,
+      // text: 'and easy to do anywhere, even with Node.js'
+      html: htmlString,
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+        res.send('Email sent')
+      })
+      .catch((error: Data) => {
+        console.error(error)
+        res.send("Failed")
+      })
+  } else {
+    res.send("Failed")
   }
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-      res.send('Email sent')
-    })
-    .catch((error: Data) => {
-      console.error(error)
-      res.send(error)
-    })
 }

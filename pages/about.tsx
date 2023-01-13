aa4f16329a8a3fff64616ff17a0117819cfe315c
Josh Gilley <jgilley@scpdist.com>
2023-01-13 13:31:43 -0500

    Updated about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -27,17 +27,15 @@ export default function About() {
             <p>
               Software engineer based out of Charleston, SC with a computer
               science degree and over 2 years of experience in the industry.
-              Proficient in a variety of programming languages including
-              JavaScript, HTML, CSS, and React.js. Experienced in developing and
-              maintaining both front-end and back-end web applications. Strong
-              problem-solving skills and the ability to effectively communicate
+              Experienced, as a full-stack developer, in maintaining both front-end and back-end web applications. 
+              Strong problem-solving skills and the ability to effectively communicate
               technical concepts to both technical and non-technical audiences.&nbsp;
             </p>
             <p>
             Able to work independently as well as part of a team, and have a
               strong focus on delivering high-quality products. In my previous
               roles, I have worked on a variety of projects ranging from small
-              business websites to large-scale e-commerce platforms. I am
+              business websites to scalable cloud computing solutions. I am
               constantly seeking to learn and stay up-to-date on the latest
               technologies and industry best practices.&nbsp;
             </p>
